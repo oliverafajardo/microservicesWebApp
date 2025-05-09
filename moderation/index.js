96d1b91a1ccdeb22b24538d6e3b7b17c49346e86
Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>
2025-05-08 18:03:42 -0700

    45: feat(moderation): Implement automated comment moderation service
    This introduces a new service to automatically moderate comments.
    
    - The moderation service processes `CommentCreated` events.
    - It rejects comments containing the word "orange" and approves others.
    - A `CommentModerated` event, including the new status (approved/rejected)
      and original comment data, is then emitted to the event bus.

--- moderation/index.js
+++ moderation/index.js
@@ -9,12 +9,23 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     const { type, data } = req.body;
 
     if (type === 'CommentCreated') {
-        const { postId, id, content } = data;
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
     }
+    res.send({});
 });
 
 app.listen(4003, () => {
