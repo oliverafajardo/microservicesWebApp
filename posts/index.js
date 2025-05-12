ae132ef14c4fc6d38586684f37928141d5d315d6
Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>
2025-05-12 15:22:26 -0700

    refactor(service-communication): Update internal service URLs for Kubernetes discovery
    This commit modifies the posts and event-bus services to use Kubernetes service names for internal communication, replacing previous endpoint configurations.
    
    - Updated `posts/index.js` to send events to `http://event-bus-srv:4005/events`.
    - Updated `event-bus/index.js` to send events to `http://posts-clusterip-srv:4000/events`.
    
    Docker images for both services (`oliverafajardo/posts` and `oliverafajardo/event-bus`) were rebuilt and pushed. The corresponding Kubernetes deployments (`posts-depl` and `event-bus-depl`) were subsequently restarted to apply the updates.

--- posts/index.js
+++ posts/index.js
@@ -29,7 +29,7 @@ app.post('/posts', async (req, res) => {
     };
 
     //send a message to the event bus to let it know that a post was created
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
@@ -46,5 +46,6 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
+    console.log('v55');
     console.log('Posts Service Listening on 4000');
 });
