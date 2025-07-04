ee9ea742d2c6b729a630069e1ddf6a0f959cc2bc
Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>
2025-07-04 11:55:43 -0700

    refactor: separate app initialization and server startup for testing
    - Create app.ts to export the Express app instance without starting the server (auth/src/app.ts)
    - Refactor index.ts to import the app and handle server startup (auth/src/index.ts)
    - This separation enables easier and cleaner integration testing using supertest by allowing tests to import the app directly without running a network server

---
+++ auth/src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import 'express-async-errors';
+import { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+
+
+import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+
+const app = express();
+
+app.set('trust proxy', true); //traffic is proxied through nginx, make sure express knows that it is secure from nginx
+
+app.use(cookieSession({
+    signed: false, //do not encrypt the cookie
+    secure: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req: Request, res: Response) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+
+export { app };
